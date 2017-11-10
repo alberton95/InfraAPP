6bef68309fda0f7db412a1621922708d137568b5
José Alberto <albertopn95@gmail.com>
2017-11-10 13:36:29 -0300

    Mudança nos Pacotes

---
+++ app/src/main/java/br/com/appinfra/appinfra/views/InsertDescriptionActivity.kt
@@ -0,0 +1,13 @@
+package br.com.appinfra.appinfra.views
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import br.com.appinfra.appinfra.R
+
+class InsertDescriptionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insert_description)
+    }
+}
